c85e30a774b21eb1f630ba4168fb943c82d87518
Misash <misash@outlook.com>
2023-07-25 17:34:45 -0500

    update notebook and script

--- svm.py
+++ svm.py
@@ -1,21 +1,13 @@
+
 import os
 import cv2
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate
-# from sklearn.model_selection import train_test_split
-# from sklearn.svm import SVC
-# from sklearn.metrics import accuracy_score, classification_report
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D,Dropout,Conv2DTranspose, UpSampling2D, concatenate
+from tensorflow.keras.models import load_model
 
-# # Establecer el dispositivo GPU
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Cambiar "0" por el número de tu GPU
-#
-# # Restringir el crecimiento de la memoria de la GPU para evitar que ocupe toda la memoria
-# physical_devices = tf.config.list_physical_devices("GPU")
-# if len(physical_devices) > 0:
-#     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 # Paso 1: Cargar los datos
@@ -26,9 +18,6 @@ mask_folder = os.path.join(data_folder, "train_masks_256")
 # Obtener lista de nombres de archivo para las imágenes
 image_files = os.listdir(image_folder)
 
-# image_files = os.listdir(image_folder)
-# half_length = len(image_files) // 2
-# image_files = image_files[:half_length]
 
 
 # Paso 2: Preparar los datos
@@ -55,11 +44,10 @@ for filename in image_files:
     mask = mask / 255.0
 
     # Cambiar los píxeles en la máscara mayores que 0 a 255
-    mask[mask > 0] = 255
+    # mask[mask > 0] = 255
 
     images.append(image)
     masks.append(mask)
-
 
 
 # Convertir las listas de imágenes y máscaras a arreglos numpy
@@ -73,74 +61,87 @@ masks = np.expand_dims(masks, axis=-1)
 
 
 
-
 # Paso 3: Implementar el modelo U-Net
-def unet(input_size=(256, 256, 1)):
-    inputs = Input(input_size)
 
-    # Contracción: encoder
-    conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+def unet(input_layer=(256, 256, 1), start_neurons=16):
+    inputs = Input(input_layer)
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(inputs)
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(0.25)(pool1)
 
-    conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
-    conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(0.5)(pool2)
 
-    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
-    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(0.5)(pool3)
 
-    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
-    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(0.5)(pool4)
 
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+    # Middle
+    convm = Conv2D(start_neurons * 16, (3, 3), activation="relu", padding="same")(pool4)
+    convm = Conv2D(start_neurons * 16, (3, 3), activation="relu", padding="same")(convm)
+    
+    deconv4 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(0.5)(uconv4)
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(uconv4)
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(uconv4)
 
-    # Expansión: decoder
-    up6 = UpSampling2D(size=(2, 2))(conv5)
-    up6 = Conv2D(512, 2, activation='relu', padding='same')(up6)
-    merge6 = concatenate([conv4, up6], axis=3)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
+    deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(0.5)(uconv3)
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(uconv3)
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(uconv3)
 
-    up7 = UpSampling2D(size=(2, 2))(conv6)
-    up7 = Conv2D(256, 2, activation='relu', padding='same')(up7)
-    merge7 = concatenate([conv3, up7], axis=3)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
+    deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+    uconv2 = Dropout(0.5)(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(uconv2)
 
-    up8 = UpSampling2D(size=(2, 2))(conv7)
-    up8 = Conv2D(128, 2, activation='relu', padding='same')(up8)
-    merge8 = concatenate([conv2, up8], axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
+    deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+    uconv1 = Dropout(0.5)(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(uconv1)
+    
+    output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
 
-    up9 = UpSampling2D(size=(2, 2))(conv8)
-    up9 = Conv2D(64, 2, activation='relu', padding='same')(up9)
-    merge9 = concatenate([conv1, up9], axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
-
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
-
-    model = Model(inputs=inputs, outputs=conv10)
-
+    model = Model(inputs=inputs, outputs=output_layer)
+    
     return model
 
 
-# Paso 4: Compilar y entrenar el modelo
-model = unet(input_size=image_size + (1,))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(images, masks, batch_size=8, epochs=1, validation_split=0.2)
+
+save_load = "load"
+model_name = "model_30_epocas.h5"
+model  = None
+
+if save_load == "save":
+    model = unet(input_layer=(256,256,1), start_neurons=32)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(images, masks, batch_size=8, epochs=10, validation_split=0.2)
+    model.save("./DataSave/" + model_name)
+else:
+    model = tf.keras.models.load_model("./DataSave/" + model_name)
 
 # Paso 5: Usar el modelo para predecir
 # Aquí puedes cargar una nueva imagen en escala de grises, redimensionarla y normalizarla de la misma manera que en el paso 2.
 # Luego, utiliza el modelo entrenado para hacer una predicción en la imagen y obtener la máscara resultante.
 
 # ejemplo de predicción
-test_image = cv2.imread("./Dataset/train_images_256/041871.000163.tif", cv2.IMREAD_GRAYSCALE)
+image_path = "./Dataset/train_images_256/041871.000163.tif"
+# image_path = "./image_input_partition/L15-0585E-0954Ngray_part134.tif"
+
+test_image = cv2.imread( image_path, cv2.IMREAD_GRAYSCALE)
 test_image = cv2.resize(test_image, image_size)
 test_image = test_image.astype('float32') / 255.0
 test_image = np.expand_dims(test_image, axis=0)
@@ -154,13 +155,14 @@ plt.title('Imagen testeada')
 plt.axis('off')
 plt.show()
 
-
-# Mostrar la máscara predicha
+#Mostrar la máscara predicha
 predicted_mask = predicted_mask.squeeze()
 plt.imshow(predicted_mask, cmap='gray')
 plt.title('Máscara predicha')
 plt.axis('off')
 plt.show()
+
+
 
 
 ##################################### SVM ##################################
